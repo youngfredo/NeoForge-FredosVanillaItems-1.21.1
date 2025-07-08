840f3c587fb66db5f11106fff3ef1d5c7e186028
youngfredo <fredbudebusiness@gmail.com>
2025-07-09 01:21:06 +0200

    -added enderite shard -added enderite ingot

--- src/main/java/com/youngfredo/morevanillaarmors/MoreVanillaArmors.java
+++ src/main/java/com/youngfredo/morevanillaarmors/MoreVanillaArmors.java
@@ -1,5 +1,8 @@
 package com.youngfredo.morevanillaarmors;
 
+import com.youngfredo.morevanillaarmors.item.ModItems;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -34,6 +37,8 @@ public class MoreVanillaArmors {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
@@ -46,7 +51,10 @@ public class MoreVanillaArmors {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.ENDERITE);
+            event.accept(ModItems.ENDERITE_SHARD);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
