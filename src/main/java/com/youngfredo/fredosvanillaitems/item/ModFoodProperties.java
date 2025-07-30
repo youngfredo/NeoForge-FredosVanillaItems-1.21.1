3e47b46e4c908519e37054fd80a825e2c2481ff9
youngfredo <fredbudebusiness@gmail.com>
2025-07-30 21:12:46 +0200

    -Changed some things

--- src/main/java/com/youngfredo/fredosvanillaitems/item/ModFoodProperties.java
+++ src/main/java/com/youngfredo/fredosvanillaitems/item/ModFoodProperties.java
@@ -26,16 +26,16 @@ public class ModFoodProperties {
             .nutrition(4)
             .saturationModifier(1.2f)
             .alwaysEdible()
-            .effect(() -> new MobEffectInstance(MobEffects.NIGHT_VISION, 4800),1.0f)
-            .effect(() -> new MobEffectInstance(MobEffects.WATER_BREATHING, 4800), 1.0f)
+            .effect(() -> new MobEffectInstance(MobEffects.HERO_OF_THE_VILLAGE, 24000),1.0f)
+            .effect(() -> new MobEffectInstance(MobEffects.LUCK, 18000), 1.0f)
             .build();
 
     public static final FoodProperties ENCHANTED_EMERALD_APPLE = new FoodProperties.Builder()
             .nutrition(4)
             .saturationModifier(1.2f)
             .alwaysEdible()
-            .effect(() -> new MobEffectInstance(MobEffects.NIGHT_VISION, 4800),1.0f)
-            .effect(() -> new MobEffectInstance(MobEffects.WATER_BREATHING, 4800), 1.0f)
-            .effect(() -> new MobEffectInstance(MobEffects.LUCK, 6000, 1), 1.0f)
+            .effect(() -> new MobEffectInstance(MobEffects.HERO_OF_THE_VILLAGE, 48000, 2),1.0f)
+            .effect(() -> new MobEffectInstance(MobEffects.LUCK, 36000), 1.0f)
+            .effect(() -> new MobEffectInstance(MobEffects.SATURATION, 18000, 4), 1.0f)
             .build();
 }
